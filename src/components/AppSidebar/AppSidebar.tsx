07875bb711c94938109c796741f11ee8c46c88f5
melievkh <khushnudmeliev@gmail.com>
2025-08-02 22:38:40 +0500

    added sidebar

--- src/components/AppSidebar/AppSidebar.tsx
+++ src/components/AppSidebar/AppSidebar.tsx
@@ -1,5 +1,71 @@
+import { Home, TableProperties, Users } from 'lucide-react';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '../ui/sidebar';
+import Link from 'next/link';
+
 const AppSidebar = () => {
-  return <div>AppSidebar</div>;
+  const items = [
+    {
+      title: 'Home',
+      url: '/',
+      icon: Home,
+    },
+    {
+      title: 'Courses',
+      url: '/',
+      icon: TableProperties,
+    },
+    {
+      title: 'Students',
+      url: '/',
+      icon: Users,
+    },
+  ];
+
+  return (
+    <Sidebar collapsible='icon'>
+      <SidebarHeader className='py-4'>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href='/'>
+                <span>Logo</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter></SidebarFooter>
+    </Sidebar>
+  );
 };
 
 export default AppSidebar;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,13 +5,14 @@ import { useTheme } from 'next-themes';
 
 import ThemeMode from './ThemeMode';
 import UserMenu from './UserMenu';
+import { SidebarTrigger } from '../ui/sidebar';
 
 const Navbar = () => {
   const { setTheme } = useTheme();
 
   return (
     <nav className='p-4 flex items-center justify-between'>
-      Collapse button
+      <SidebarTrigger />
       <div className='flex items-center gap-4'>
         <Link href='/'>Dashboard</Link>
         <ThemeMode setTheme={setTheme} />
